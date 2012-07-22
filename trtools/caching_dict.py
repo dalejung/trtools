af91f39660a1363cccd83271b5f513878975826c
Dale Jung <dale@dalejung.com>
2012-07-21 22:22:55 -0400

    moved CachingDict to own and added some tests

---
+++ trtools/caching_dict.py
@@ -0,0 +1,50 @@
+import cPickle as pickle
+import os.path
+from contextlib import closing
+
+
+class CachingDict(object):
+    """
+        Essentially a dict that writes itself to disk everytime an
+        item is set
+    """
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self._dict = {}
+
+        self.load()
+
+    def keys(self):
+        return self._dict.keys()
+
+    def update(self, *args, **kwargs):
+        self._dict.update(*args, **kwargs)
+        self.save()
+
+    def __setitem__(self, key, value):
+        self._dict.__setitem__(key, value)
+        self.save()
+
+    def __getitem__(self, key):
+        return self._dict.__getitem__(key)
+
+    def load(self):
+        try:
+            obj = pickle.load(self.get_fp())
+            self._dict = obj
+        except:
+            pass
+
+    def get_fp(self, mode='rb'):
+        """
+            Factored out for testing purposes
+        """
+        return open(self.filepath, mode)
+
+    def save(self):
+        with closing(self.get_fp('wb')) as f:
+            pickle.dump(self, f)
+
+    def clear(self):
+        self._dict.clear()
+        self.save()
---
+++ trtools/tests/test_caching_dict.py
@@ -0,0 +1,97 @@
+from unittest import TestCase
+import os.path
+from StringIO import StringIO
+
+
+import trtools.caching_dict as cdict
+
+def curpath():
+    pth, _ = os.path.split(os.path.abspath(__file__))
+    return pth
+
+class FakeFile(StringIO):
+    """
+        Pretty Flimsy fake file. 
+        assumes all writes are non-append
+    """
+    _data = ''
+
+    def close(self):
+        pass
+
+    def write(self, data):
+        FakeFile._data = data
+        StringIO.flush(self)
+        StringIO.write(self, data)
+
+class TestDict(cdict.CachingDict):
+    fakefile = FakeFile()
+
+    def __init__(self):
+        super(TestDict, self).__init__('test')
+
+    def get_fp(self, mode='rb'):
+        TestDict.fakefile.seek(0)
+        return TestDict.fakefile
+
+    def clear(self):
+        TestDict.fakefile.flush()
+        super(TestDict, self).clear()
+
+
+class TestCachingDict(TestCase):
+
+    def __init__(self, *args, **kwargs):
+        TestCase.__init__(self, *args, **kwargs)
+
+    def runTest(self):
+        pass
+
+    def setUp(self):
+        pass
+
+    def test_save(self):
+        cd = TestDict()
+        cd['whee'] = 'hello'
+        cd['boo'] = 'boo'
+
+        cd2 = TestDict()
+        assert cd2['whee'] == 'hello'
+        assert cd2['boo'] == 'boo'
+
+    def test_update(self):
+        cd = TestDict()
+        cd.clear()
+        assert len(cd.keys()) == 0
+        cd.update(bob='whee', tom='kat')
+        assert cd['bob'] == 'whee'
+        assert cd['tom'] == 'kat'
+
+        cd2 = TestDict()
+        assert cd2['bob'] == 'whee'
+        assert cd2['tom'] == 'kat'
+
+    def test_clear(self):
+        cd = TestDict()
+        cd.update(bob='whee', tom='kat')
+        assert cd['bob'] == 'whee'
+        assert cd['tom'] == 'kat'
+        cd.clear()
+        assert len(cd.keys()) == 0
+        assert 'bob' not in cd.keys()
+
+    def test_not_in(self):
+        """
+            Make sure that not in tests the keys()
+            not sure how to do this atm
+        """
+        cd = TestDict()
+        cd.clear()
+        assert len(cd.keys()) == 0
+        assert 'bob' not in cd
+
+
+
+if __name__ == '__main__':                                                                                          
+    import nose                                                                      
+    nose.runmodule(argv=[__file__,'-vvs','-x','--pdb', '--pdb-failure'],exit=False)   
