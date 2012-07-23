4e5ef8fcc868274da0b8f2c29cfa4b169b2a5848
Dale Jung <dale@dalejung.com>
2012-07-23 17:54:11 -0400

    load and save funcs that accept file objects and paths

---
+++ trtools/io.py
@@ -0,0 +1,23 @@
+import cPickle as pickle
+
+def save(obj, path_or_buffer):
+    if isinstance(path_or_buffer, basestring):
+        f = open(basestring, 'wb')
+    else:
+        f = path_or_buffer
+
+    try:
+        return pickle.dump(obj, f, protocol=pickle.HIGHEST_PROTOCOL)
+    finally:
+        f.close()
+
+def load(path_or_buffer):
+    if isinstance(path_or_buffer, basestring):
+        f = open(basestring, 'rb')
+    else:
+        f = path_or_buffer
+
+    try:
+        return pickle.load(f)
+    finally: 
+        f.close()
