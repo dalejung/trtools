de195f96f496f642f4cc45e40a2257c13f3a4e57
Dale Jung <dale@dalejung.com>
2012-08-10 22:29:55 -0400

    added more datetime logic.

--- trtools/rpy/conversion.py
+++ trtools/rpy/conversion.py
@@ -1,10 +1,10 @@
 import sys, copy, os, itertools
+import time
 
 import rpy2.robjects as robjects
 import rpy2.rinterface as rinterface
-from rpy2.robjects.vectors import SexpVector, ListVector
+from rpy2.robjects.vectors import SexpVector, ListVector, StrSexpVector
 from rpy2.robjects.robject import RObjectMixin, RObject
-import rpy2.robjects as robj
 import numpy as np
 import pandas.rpy.common as rcom
 import pandas as pd
@@ -15,7 +15,10 @@ VECTOR_TYPES = rcom.VECTOR_TYPES
 baseenv_ri = rinterface.baseenv
 globalenv_ri = rinterface.globalenv
 
-def my_ri2py(o):
+# Probably need better logic to detect if xts was imported
+robjects.r('require(xts)')
+
+def pd_ri2py(o):
     res = None
     try:
         rcls = o.do_slot("class")
@@ -24,7 +27,10 @@ def my_ri2py(o):
 
     if isinstance(o, SexpVector):
         if 'xts' in rcls:
+            print 'xts'
             res = convert_xts_to_df(o)
+        if 'POSIXct' in rcls:
+            res = convert_posixct_to_index(o)
 
     if res is None:
         res = robjects.default_ri2py(o)
@@ -42,24 +48,117 @@ def convert_xts_to_df(o):
     df.index = dates
     return df
 
-robjects.conversion.ri2py = my_ri2py
+def convert_posixct_to_index(o):
+    """
+        Convert a POSIXct object to a DatetimeIndex.
+    """
+    tz = o.do_slot('tzone')[0]
+    dates = np.array(o, dtype=np.dtype("M8[s]"))
+    index = pd.DatetimeIndex(dates, tz='UTC')
+    index = index.tz_convert(tz)
+    return index
+
+robjects.conversion.ri2py = pd_ri2py
 
 def pd_py2ri(o):
     """ 
     """
-
     res = None
-    if isinstance(o, pd.DataFrame) and isinstance(o.index, pd.DatetimeIndex):
-        res = convert_df_to_xts(o)
+    if isinstance(o, pd.DataFrame): 
+        if isinstance(o.index, pd.DatetimeIndex):
+            res = convert_df_to_xts(o)
+        else:
+            res = rcom.convert_to_r_dataframe(o)
 
+    if isinstance(o, pd.DatetimeIndex): 
+        res = convert_datetime_index(o)
+        
     if res is None:
         res = robjects.default_py2ri(o)
 
     return res
 
+def convert_dataframe_columns(df, strings_as_factors=False):
+    """
+    Essentially the same as pandas.spy.common.convert_to_r_dataframe
+    except we don't convert the index into strings
+
+
+    We are just grabbing the column data here
+    """
+
+    import rpy2.rlike.container as rlc
+
+    columns = rlc.OrdDict()
+
+    #FIXME: This doesn't handle MultiIndex
+
+    for column in df:
+        value = df[column]
+        value_type = value.dtype.type
+        value = [item if pd.notnull(item) else NA_TYPES[value_type]
+                 for item in value]
+
+        value = VECTOR_TYPES[value_type](value)
+
+        if not strings_as_factors:
+            I = robjects.baseenv.get("I")
+            value = I(value)
+
+        columns[column] = value
+
+    r_dataframe = robjects.DataFrame(columns)
+    return r_dataframe
+
+def _localize_tz(df):
+    """
+        We don't have the option of naive timestamps in R. 
+        If we pass in timezone-less data, it will be converted to our
+        system's TZ. This will result in silent errors since R doesn't raise
+        a NonExistentTimeError.
+    """
+    ind = df.index
+    if ind.tz is None:
+        ind = ind.tz_localize('UTC')
+    df.index = ind
+    return df
+
 def convert_df_to_xts(df, strings_as_factors=False):
+    df = _localize_tz(df)
     r_dataframe = XTS(df)
     return r_dataframe
+
+def convert_datetime_index_string(ind):
+    """
+        Convert to POSIXct via strings
+    """
+    rownames = robjects.StrVector(ind)
+    tz = ind.tz.zone
+    # convert m8[ns] = ms8[s]
+    asposix = robjects.r.get('as.POSIXct')
+    return asposix(rownames, origin="1970-01-01", tz=tz)
+
+def convert_datetime_index_num(ind):
+    """
+        Convert to POSIXct using m8[s] format
+        see robject.vectors.POSIXct where I grabbed logic
+    """
+    # convert m8[ns] to m8[s]
+    vals = robjects.vectors.FloatSexpVector(ind.asi8 / 1E9)
+    as_posixct = baseenv_ri['as.POSIXct']
+    origin = StrSexpVector([time.strftime("%Y-%m-%d", 
+                                          time.gmtime(0)),])
+    # We will be sending ints as UTC
+    tz = ind.tz and ind.tz.zone or 'UTC'
+    tz = StrSexpVector([tz])
+    utc_tz = StrSexpVector(['UTC'])
+
+    posixct = as_posixct(vals, origin=origin, tz=utc_tz)
+    posixct.do_slot_assign('tzone', tz)
+    return posixct
+
+def convert_datetime_index(o):
+    return POSIXct(o)
 
 class XTS(RObject):
     """ R 'as.xts'.
@@ -70,14 +169,29 @@ class XTS(RObject):
         """
         self.rdf = None
         if isinstance(df, pd.DataFrame):
-            rdf = rcom.convert_to_r_dataframe(df)
-            self.rdf = rdf
-            xts = baseenv_ri.get("as.xts").rcall(tuple([('x', rdf)]), globalenv_ri)
+            rdf = convert_dataframe_columns(df)
+            ind = convert_datetime_index_num(df.index)
+            kv = [('x', rdf), ('order.by', ind)]
+            kv = tuple(kv)
+            xts = baseenv_ri.get("as.xts").rcall(kv, globalenv_ri)
             super(XTS, self).__init__(xts)
         else:
             raise ValueError("Currently only supporting DataFrames")
     
     def __repr__(self):
         return self.rdf.__repr__()
+
+class POSIXct(robjects.vectors.FloatVector):
+    """ R 'as.POSIXct'.
+    """
+    
+    def __init__(self, ind):
+        """ Create a POSIXct.
+        """
+        if isinstance(ind, pd.DatetimeIndex):
+            posixct = convert_datetime_index_num(ind)
+            super(POSIXct, self).__init__(posixct)
+        else:
+            raise ValueError("Currently only supporting DataFrames")
 
 robjects.conversion.py2ri = pd_py2ri
---
+++ trtools/rpy/tests/test_conversion.py
@@ -0,0 +1,85 @@
+from unittest import TestCase
+import os.path
+
+import os
+
+import pandas as pd
+import numpy as np
+from rpy2.robjects import r
+from rpy2.robjects.vectors import SexpVector, ListVector, StrSexpVector
+
+import trtools.util.testing as tm
+
+import trtools.rpy.conversion as conv
+
+# Make sure to set R_HOME in environ
+
+def curpath():
+    pth, _ = os.path.split(os.path.abspath(__file__))
+    return pth
+
+class TestConversion(TestCase):
+
+    def __init__(self, *args, **kwargs):
+        TestCase.__init__(self, *args, **kwargs)
+
+    def runTest(self):
+        pass
+
+    def setUp(self):
+        pass
+
+    def test_dataframe(self):
+        ind = pd.date_range(start='1/1/2002', end='12/30/2008', freq="30min")
+
+        # test converting a dataframe with 1 col. This errored while
+        # multiple columns worked fine
+        df = pd.DataFrame({'returns': np.random.randn(len(ind))}, index=ind)
+        conv.convert_df_to_xts(df)
+
+
+        # test with two columns
+        df['bob'] = 3
+        conv.convert_df_to_xts(df)
+
+    def test_convert_datetime(self):
+        ind = pd.date_range(start='1/1/2002', end='12/30/2008', freq="30min")
+        ri = conv.convert_datetime_index_num(ind)
+        tm.assert_almost_equal(ri[0], ind.asi8[0] / 1E9)
+
+        assert ri.do_slot('tzone')[0] == 'UTC'
+
+        # convert back to datetime
+        conv_dates = pd.Series(ri).astype(np.dtype('M8[s]'))
+        tm.assert_almost_equal(conv_dates, ind)
+
+    def test_convert_posixct_to_index(self):
+        ind = pd.date_range(start='1/1/2002', end='12/30/2008', freq="30min")
+        ri = conv.convert_datetime_index_num(ind)
+
+        # test converting back 'UTC'
+        dt = conv.convert_posixct_to_index(ri)
+        tm.assert_almost_equal(dt, ind,)
+
+        # test 'US/Eastern'
+        # At one point this failed due to DatetimeIndex being 
+        # given a tz. DatetimeIndex needs to be initialied to UTC
+        # and then converted
+        ri.do_slot_assign('tzone', StrSexpVector(['US/Eastern']))
+        dt = conv.convert_posixct_to_index(ri)
+        assert dt.tz.zone == 'US/Eastern'
+        assert ind.tz is None
+        tm.assert_almost_equal(dt, ind)
+
+        ind = ind.tz_localize('UTC')
+        est_ind = ind.tz_convert('US/Eastern')
+        est_ri = conv.convert_datetime_index_num(est_ind)
+        assert est_ri.do_slot('tzone')[0] == 'US/Eastern'
+        est_dt = conv.convert_posixct_to_index(est_ri)
+        tm.assert_almost_equal(est_dt, est_ind)
+        assert est_dt.tz.zone == 'US/Eastern'
+
+
+if __name__ == '__main__':                                                                                          
+    import nose                                                                      
+    nose.runmodule(argv=[__file__,'-vvs','-x','--pdb', '--pdb-failure'],exit=False)   
