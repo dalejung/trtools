849124d30eda4ab44327062e4089d0369d92860d
Dale Jung <dale@dalejung.com>
2012-08-27 16:25:51 -0400

    removed out param

--- trtools/core/binning.py
+++ trtools/core/binning.py
@@ -8,18 +8,19 @@ from pandas.core.groupby import DataFrameGroupBy, SeriesGroupBy, BinGrouper
 import trtools.core.timeseries as ts # downsample moneky patch
 from trtools.monkey import patch, patch_prop
 
-def apply_put_series(data, grouper, func, out=None, *args, **kwargs):
+def apply_put_series(data, grouper, func, *args, **kwargs):
 
     # grab first result to find dtype
     first = grouper.bins[0]
-    res = func(data[0:first])
-    if out is None:
-        out = np.empty(len(data), dtype=res.dtype)  
+    res = func(data[0:first], *args, **kwargs)
+    out = np.empty(len(data), dtype=res.dtype)  
     out[0:first] = res
 
     start = first
     for bin in grouper.bins[1:]:
-        out[start:bin] = func(data[start:bin])
+        if start == bin:
+            continue
+        out[start:bin] = func(data[start:bin], *args, **kwargs)
         start = bin
     return pd.Series(out, index=data.index) 
 
@@ -33,7 +34,19 @@ def apply_put_frame(data, grouper, func, *args, **kwargs):
         if col not in func:
             continue
         f = func[col]
-        s = apply_put_series(data[col], grouper, f)
+        s = apply_put_series(data[col], grouper, f, *args, **kwargs)
+        sdict[col] = s
+
+    return pd.DataFrame(sdict, columns=sdict.keys(),index=data.index)
+
+# TODO make a version that passes dataframe to func
+def apply_put_frame2(data, grouper, func, *args, **kwargs):
+    sdict = OrderedDict() 
+    for col in data.columns:
+        if col not in func:
+            continue
+        f = func[col]
+        s = apply_put_series(data[col], grouper, f, *args, **kwargs)
         sdict[col] = s
 
     return pd.DataFrame(sdict, columns=sdict.keys(),index=data.index)
