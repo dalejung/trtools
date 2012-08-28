9b9493fbdf7b1a6c1ad35655bec74a9056f0f642
Dale Jung <dale@dalejung.com>
2012-08-28 18:55:27 -0400

    had to add chunker to break up jobs. Queue max size is 32k

--- trtools/tools/parallel.py
+++ trtools/tools/parallel.py
@@ -1,35 +1,48 @@
 import multiprocessing as mp
+import math
 
 default_consumers = mp.cpu_count() * 2 
+
+def chunker(seq, size):
+        return (seq[pos:pos + size] for pos in xrange(0, len(seq), size))
+
+# OSX Max Queue Size
+MAX_SIZE = 32000
 
 def farm(tasks, num_consumers=None, verbose=False):
     if num_consumers is None:
         num_consumers = default_consumers
-    task_queue = mp.JoinableQueue()
+    task_queue = mp.Queue()
     result_queue = mp.Queue()
 
     # Start consumers
     print 'Creating %d consumers' % num_consumers
-    consumers = [ DataProcess(task_queue, result_queue, verbose)
+    consumers = [ DataProcess(task_queue, result_queue, verbose=verbose)
                   for i in xrange(num_consumers) ]
 
     num_jobs = len(tasks)
     print 'Creating Task Queue %d items' % num_jobs
     # fill queue
-    for task in tasks:
-        task_queue.put(task)
 
+    if len(tasks) > MAX_SIZE:
+        tasks = chunker(tasks, int(math.ceil(len(tasks)*1.0/MAX_SIZE)))
+    else:
+        tasks = chunker(tasks, 1)
+
+    for task in tasks:
+        task_queue.put(task, False)
+
+    print 'starting process'
     start_process(task_queue, consumers)
 
     # Start printing results
-    print 'printing results'
     results = []
     while num_consumers:
         result = result_queue.get()
         if result is None:
             num_consumers -= 1
             continue
-        results.append(result)       
+        results.extend(result)       
 
     return results
 
@@ -44,7 +57,7 @@ def start_process(task_queue, consumers):
         task_queue.put(None)
 
     # Wait for the worker to finish
-    task_queue.join()
+    #task_queue.join()
 
 class Task(object):
     def __init__(self, func, job):
@@ -79,14 +92,18 @@ class DataProcess(mp.Process):
         self.current_task = task
         if task is None:
             # Poison pill means we should exit
-            self.task_queue.task_done()
+            #self.task_queue.task_done()
             return False
         if self.verbose:
             print str(self)+'Processing '+str(task)+' Jobs Complete: '+str(self.jobs_complete)
-        data = task()
-        self.task_queue.task_done()
-        if data is not None:
-            print 'put data'
+
+        data = []
+        for t in task:
+            d = t()
+            if d is not None:
+                data.append(d)
+        #self.task_queue.task_done()
+        if len(data) > 0:
             self.result_queue.put(data)
         return True
 
