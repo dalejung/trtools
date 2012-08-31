bb0320c573bcd56567cf95f66de740a5f548de4e
Dale Jung <dale@dalejung.com>
2012-08-31 16:22:40 -0400

    better slicing and indexing, concentrating more on One Big Table implementation

--- trtools/io/pytables.py
+++ trtools/io/pytables.py
@@ -6,6 +6,7 @@ from tables import *
 import pandas as pd
 import pandas.lib as lib
 import numpy as np
+import pandas.io.pytables as pdtables
 
 from trtools.io.common import _filename
 
@@ -18,18 +19,22 @@ def convert_frame(df):
     """
     sdict = OrderedDict()
     atoms = OrderedDict()
+    types = OrderedDict()
 
     #index
     index_name = df.index.name or 'pd_index'
-    converted, atom = _convert_obj(df.index)
+    converted, inferred_type, atom = _convert_obj(df.index)
     atoms[index_name] = atom
     sdict[index_name] = converted
+    types[index_name] = inferred_type
+
 
     # columns
     for col in df.columns:
-        converted, atom = _convert_obj(df[col])
+        converted, inferred_type, atom = _convert_obj(df[col])
         atoms[col] = atom
         sdict[col] = converted
+        types[col] = inferred_type
 
     # create table desc
     desc = {}
@@ -43,22 +48,28 @@ def convert_frame(df):
     recs = np.recarray(shape=len(df), dtype=dtypes)
     for k, v in sdict.items():
         recs[k] = v
-    return desc, recs
+    return desc, recs, types
 
 def _convert_obj(obj):
+    """
+        Convert a series to pytables values and Atom
+    """
     if isinstance(obj, pd.DatetimeIndex):
         converted = obj.asi8
-        return converted, Int64Atom()
-    elif isinstance(obj, (pd.Int64Index, pd.PeriodIndex)):
+        return converted, 'datetime64', Int64Atom()
+    elif isinstance(obj, pd.PeriodIndex):
         converted = obj.values
-        return converted, Int64Atom()
+        return converted, 'periodindex', Int64Atom()
+    elif isinstance(obj, pd.PeriodIndex):
+        converted = obj.values
+        return converted, 'int64', Int64Atom()
 
     inferred_type = lib.infer_dtype(obj)
     values = np.asarray(obj)
 
     if inferred_type == 'datetime64':
         converted = values.view('i8')
-        return converted, Int64Atom()
+        return converted, inferred_type, Int64Atom()
     if inferred_type == 'string':
         converted = np.array(list(values), dtype=np.str_)
         itemsize = converted.dtype.itemsize
@@ -67,56 +78,27 @@ def _convert_obj(obj):
         if itemsize < MIN_ITEMSIZE:
             itemsize = MIN_ITEMSIZE
             converted = converted.astype("S{0}".format(itemsize))
-        return converted, StringAtom(itemsize)
+        return converted, inferred_type, StringAtom(itemsize)
     elif inferred_type == 'unicode':
         # table's don't seem to support objects
         raise Exception("Unsupported inferred_type {0}".format(inferred_type))
 
         converted = np.asarray(values, dtype='O')
-        return converted, ObjectAtom()
+        return converted, inferred_type, ObjectAtom()
     elif inferred_type == 'datetime':
-        return Time64Atom()
+        converted = np.array([(time.mktime(v.timetuple()) +
+                            v.microsecond / 1E6) for v in values],
+                            dtype=np.float64)
+        return converted, inferred_type, Time64Atom()
     elif inferred_type == 'integer':
         converted = np.asarray(values, dtype=np.int64)
-        return converted, Int64Atom()
+        return converted, inferred_type, Int64Atom()
     elif inferred_type == 'floating':
         converted = np.asarray(values, dtype=np.float64)
-        return converted, Float64Atom()
+        return converted, inferred_type, Float64Atom()
 
     raise Exception("Unsupported inferred_type {0}".format(inferred_type))
     
-
-def _get_atom(obj, check_string=False):
-    if isinstance(obj, pd.DatetimeIndex):
-        return Int64Atom()
-    elif isinstance(obj, (pd.Int64Index, pd.PeriodIndex)):
-        return Int64Atom()
-
-    inferred_type = lib.infer_dtype(obj)
-
-    if inferred_type == 'datetime64':
-        return Int64Atom()
-    if inferred_type == 'string':
-        # unless we're indexing, we don't care about string?
-        if not check_string:
-            return ObjectAtom()
-        converted = np.array(list(obj.values), dtype=np.str_)
-        itemsize = converted.dtype.itemsize
-        # for OBT, can't assume value will be right for future
-        # frame keys
-        itemsize = max(itemsize, MIN_ITEMSIZE)
-        return StringAtom(itemsize)
-    elif inferred_type == 'unicode':
-        return ObjectAtom()
-    elif inferred_type == 'datetime':
-        return Time64Atom()
-    elif inferred_type == 'integer':
-        return Int64Atom()
-    elif inferred_type == 'floating':
-        return Float64Atom()
-    
-    return ObjectAtom()
-
 def _meta(obj, meta=None):
     if meta:
         obj._v_attrs.pd_meta = meta
@@ -127,13 +109,6 @@ def _meta(obj, meta=None):
     except:
         return {}
 
-def get_col(obj, pos=None):
-    """
-        Get the tables.Col from dtype
-    """
-    atom = _get_atom(obj)
-    return Col.from_atom(atom, pos=pos) 
-
 def _name(table):
     try:
         name = table.attrs.pandas_name
@@ -143,7 +118,7 @@ def _name(table):
 
 def _columns(table):
     try:
-        columns = table.attrs.pandas_columns
+        columns = _meta(table)['columns']
     except:
         # assume first is index
         columns = table.colnames[1:]
@@ -151,61 +126,35 @@ def _columns(table):
 
 def _index_name(table):
     try:
-        index_name = table.attrs.pandas_index_name
+        index_name = _meta(table)['index_name']
     except:
         # assume first is index
         index_name = table.colnames[0]
     return index_name
 
-def _index_type(table):
-    try:
-        index_type = table.attrs.pandas_index_type
-    except:
-        iname = _index_name(table)
-        index_type = getattr(table.cols, iname).dtype
-        if index_type.type == np.int64 and 'time' in iname:
-            index_type = np.dtype('M8[ns]')
+def unconvert_obj(values, type):
+    if type == 'datetime64':
+        return values.astype("M8[ns]")
 
-    return index_type
+    return values
 
-def frame_description(df):
-    """
-        Generate the tables.Table description from DataFrame
-        Keeps track of positions, though we store df.columns
-        as well
-    """
-    desc = {}
-    pos = 0
+def unconvert_index(index_values, type):
+    return pdtables._unconvert_index(index_values, type)
 
-    #index
-    index_name = df.index.name or 'pd_index'
-    desc[index_name] = get_col(df.index, pos)
-    pos += 1
-
-    # columns
-    for col in df.columns:
-        desc[col] = get_col(df[col], pos)
-        pos += 1
-    return desc
-
-def convert_index(index):
-    if isinstance(index, pd.DatetimeIndex):
-        return index.asi8
-
-def unconvert_index(index_values, index_dtype):
-    if index_dtype.type == np.datetime64:
-        return pd.DatetimeIndex(index_values)
-
-def create_table_from_frame(name, df, hfile, hgroup, desc):
+def create_table_from_frame(name, df, hfile, hgroup, desc, types):
     with warnings.catch_warnings(): # ignore the name warnings
         warnings.simplefilter("ignore")
         table = hfile.createTable(hgroup, _filename(name), desc, str(name),
                                   expectedrows=len(df))
 
-    table.attrs.pandas_columns = df.columns
-    table.attrs.pandas_index_name = df.index.name or 'pd_index'
-    table.attrs.pandas_index_type = df.index.dtype
-    table.attrs.pandas_name = name
+    meta = {}
+    meta['columns'] = df.columns
+    meta['value_types'] = types
+    meta['index_name'] = df.index.name or 'pd_index'
+    meta['name'] = name
+
+    _meta(table, meta)
+
     return table
 
 def frame_to_table(df, hfile, hgroup, name=None):
@@ -219,12 +168,9 @@ def frame_to_table(df, hfile, hgroup, name=None):
         series_name = 'vals'
         df = pd.DataFrame({series_name:df}, index=df.index)
 
-    desc, recs = convert_frame(df)
-    table = create_table_from_frame(name, df, hfile, hgroup, desc)
-
-    # TODO this is fast but can't support multiindex
+    desc, recs, types = convert_frame(df)
+    table = create_table_from_frame(name, df, hfile, hgroup, desc, types)
     table.append(recs)
-
     hfile.flush()
 
 def table_to_frame(table, where=None):
@@ -233,27 +179,33 @@ def table_to_frame(table, where=None):
     """
     columns = _columns(table)
     index_name = _index_name(table)
-    index_type = _index_type(table)
     name = _name(table)
 
+    meta = _meta(table)
+    types = meta.setdefault('value_types', {})
 
     if where:
-        data = table.readWhere(where)
+        try:
+            print "Where Clause: {0}\n".format(where)
+            data = table.readWhere(where)
+        except Exception as err:
+            raise Exception("readWhere error: {0} {1}".format(where, str(err)))
     else:
         data = table.read()
 
-    df = table_data_to_frame(data, columns, index_name, index_type)
-
+    df = table_data_to_frame(data, columns, index_name, types)
     df.name = name
     return df
 
-def table_data_to_frame(data, columns, index_name, index_type):
+def table_data_to_frame(data, columns, index_name, types):
     index_values = data[index_name]
-    index = unconvert_index(index_values, index_type)
+    index = unconvert_index(index_values, types[index_name])
 
     sdict = {}
     for col in columns:
-        sdict[col] = data[col]
+        temp = data[col]
+        temp = unconvert_obj(temp, types[col])
+        sdict[col] = temp
 
     df = pd.DataFrame(sdict, columns=columns, index=index)
     return df
@@ -276,6 +228,9 @@ class HDFPanel(object):
     def close(self):
         if self.handle is not None and self.handle.isopen:
             self.handle.close()
+
+    def keys(self):
+        return self.groups()
 
     def groups(self):
         handle = self.handle
@@ -303,17 +258,23 @@ class HDFPanel(object):
         return self.get_group(key)
 
     def create_group(self, group_name):
+        """
+            Create HDFPanelGroup
+        """
         handle = self.handle    
         group = handle.createGroup(handle.root, group_name, group_name)
 
         meta = {}
         meta['group_type'] = 'panel'
 
-        group._v_attrs.pd_meta = meta
+        _meta(group, meta)
 
         return HDFPanelGroup(group, self)
 
     def create_obt(self, group_name, frame_key=None, table_name=None):
+        """
+            Create OBTGroup
+        """
         handle = self.handle    
         group = handle.createGroup(handle.root, group_name, group_name)
 
@@ -325,7 +286,7 @@ class HDFPanel(object):
         meta['table_name'] = table_name
         meta['group_type'] = 'obt'
 
-        group._v_attrs.pd_meta = meta
+        _meta(group, meta)
 
         return OBTGroup(group, self, **meta)
 
@@ -343,9 +304,9 @@ class HDFPanelGroup(object):
 
         raise Exception("Name does not exist in this Group")
 
-    def get_data(self, name, start=None, end=None):
+    def get_data(self, name):
         table = self.get_table(name)
-        df = self._get_data(table, start, end)
+        df = table_to_frame(table, where=where)
         return df
 
     def get_all(self, start=None, end=None):
@@ -354,26 +315,6 @@ class HDFPanelGroup(object):
             df = self._get_data(node, start, end)
             ret[node._v_name] = df
         return ret
-            
-    def _get_data(self, table, start=None, end=None):
-        index_name = _index_name(table)
-        where = []
-        if start:
-            start = pd.Timestamp(start).value
-            start_where = "({0} > {1})".format(index_name, start)
-            where.append(start_where)
-        if end:
-            end = pd.Timestamp(end).value
-            end_where = "({0} < {1})".format(index_name, end)
-            where.append(end_where)
-
-        if len(where) > 0:
-            where = " & ".join(where)
-        else:
-            where = None
-
-        df = table_to_frame(table, where=where)
-        return df
 
     def keys(self):
         return self.group._v_children.keys()
@@ -385,7 +326,7 @@ class HDFPanelGroup(object):
 
     def append(self, df, name=None):
         table = self.get_table(name)
-        desc, recs = convert_frame(df)
+        desc, recs, types = convert_frame(df)
         table.append(recs)
 
     def __setitem__(self, key, value):
@@ -411,6 +352,8 @@ class OBTGroup(HDFPanelGroup):
         self._table = None
 
     def __setitem__(self, key, value):
+        # TODO This should be like mode 'w'. Where another method will be the append
+        # Right now it will just append to the same frame_key
         value[self.frame_key] = key
         table_name = self.table_name
         if hasattr(self.group, table_name):
@@ -422,15 +365,25 @@ class OBTGroup(HDFPanelGroup):
     def __getitem__(self, key):
         # TODO This can be faster if we cache the getWhereList somewhere on disk
 
-        if isinstance(key, basestring):
-            key = "'{0}'".format(key)
+        key = _convert_param(key)
+        if isinstance(key, HDFQuery):
+            return self._getitem_query(key)
+        if isinstance(key, slice):
+            raise NotImplementedError('TODO work on slicing')
+        return self._getitem_framekey(key)
+
+    def _getitem_framekey(self, key):
         where = "{0} == {1}".format(self.frame_key, key)
         df = table_to_frame(self.table, where=where)
         del df[self.frame_key]
         return df
 
-    def get_all(self, start=None, end=None):
-        # not sure if this should be a dict of DFs
+    def _getitem_query(self, query):
+        where = str(query)
+        df = table_to_frame(self.table, where=where)
+        return df
+
+    def get_all(self):
         all_df = table_to_frame(self.table)
         return all_df
 
@@ -449,33 +402,65 @@ class OBTGroup(HDFPanelGroup):
         data = self.table.col(self.frame_key)
         return list(np.unique(data))
 
+    def __repr__(self):
+        return repr(self.table)
 
-def _convert_param(param):
-    if isinstance(param, basestring):
+
+def _convert_param(param, base_type=None):
+    """
+        A well not thought out function to convert params to the proper base type. 
+    """
+    if base_type == 'datetime64' and isinstance(param, basestring):
+        return pd.Timestamp(param).value
+
+    if isinstance(param, basestring): # quote the string params
         param = "'{0}'".format(param)
 
-    if isinstance(param, pd.Timestamp):
+    if isinstance(param, pd.Timestamp): # Timestamp itself is never valid type
         param = param.value
 
     return param
 
 class HDFSql(object):
+    """
+        HDFSql object. Kept in separate obj so we don't polute __getattr__ on table
+    """
     def __init__(self, table):
         self.table = table
+        # assuming meta won't change during object lifetime...
+        self.meta = _meta(table)
+        self.types = self.meta['value_types']
 
     def __getattr__(self, key):
+        key = self.get_valid_key(key)
+        try: 
+            type = self.types[key]
+        except:
+            type = None
+
+        return HDFQuery(key, type)
+        raise AttributeError("No column")
+
+    def get_valid_key(self, key):
         if key in self.table.colnames:
-            return HDFQuery(key)
+            return key
+        # shortcuts
+        if key == 'index':
+            return _index_name(self.table)
+        if key == 'items':
+            return self.frame_key
         raise AttributeError("No column")
 
 class HDFQuery(object):
 
-    def __init__(self, base):
+    def __init__(self, base, base_type=None):
         self.base = base
+        self.base_type = base_type
 
     def base_op(self, other, op):
-        base = "{0} {1} {2}".format(self.base, op, _convert_param(other))
-        return HDFQuery(base)
+        """ quick convert to pytable expression """
+        base = "{0} {1} {2}".format(self.base, op, _convert_param(other, self.base_type))
+        return HDFQuery(base, 'statement')
 
     __eq__  = lambda self, other: self.base_op(other, "==")
     __gt__  = lambda self, other: self.base_op(other, ">")
