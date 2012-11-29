1a82e7bb5e96c67c0a3578199ec586d82dd97c7c
Dale Jung <dale@dalejung.com>
2012-11-28 21:01:59 -0500

    added a table function for summary statistics

--- trtools/core/wrangling.py
+++ trtools/core/wrangling.py
@@ -1,3 +1,6 @@
+from collections import OrderedDict
+
+import pandas as pd
 from pandas import Panel, DataFrame, MultiIndex, Series
 
 from trtools.monkey import patch
@@ -72,7 +75,31 @@ def foreach_dataframe(self, func, force_dict=False, *args, **kwargs):
     res = res.sort()
     return res
 
-
 @patch(DataFrame, 'dataset')
 def dataset(self):
     return DataFrame(index=self.index)
+
+@patch([Series, DataFrame], 'table')
+def table_agg(self, funcs):
+    """
+        Good for summary statistics.
+
+        >>> df.table(['mean', 'sum'])
+        
+        Will return a summary DataFrame like:
+            'mean'   'sum'
+       col1   2        200
+       col2   3        300
+    """
+    # list of func names
+    if isinstance(funcs, list):
+        funcs = OrderedDict([(x,x) for x in funcs])
+
+    data = OrderedDict()
+    for k, f in funcs.items():
+        func = f
+        if isinstance(f, basestring):
+            func = lambda df: getattr(df, f)()
+        data[k] = func(self)
+        
+    return pd.DataFrame(data, columns=data.keys())
