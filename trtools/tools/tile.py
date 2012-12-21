492c9e641e859d62746495d4c1103453c049b669
Dale Jung <dale@dalejung.com>
2012-12-20 19:31:45 -0500

    changed the tiel stuff to return a Categorical.

--- trtools/tools/tile.py
+++ trtools/tools/tile.py
@@ -1,6 +1,7 @@
 from __future__ import division
+import operator
 
-from pandas import cut, Series, DataFrame
+from pandas import cut, Series, DataFrame, Categorical
 import numpy as np
 
 from trtools.monkey import patch
@@ -27,18 +28,32 @@ class NumRange(object):
             return self.l
         return (self.l + self.h) / 2
 
-    __eq__  = lambda self, other: self.l == other.l
-    __ne__  = lambda self, other: self.l != other.l
-    __lt__  = lambda self, other: self.l < other.l
-    __le__  = lambda self, other: self.l <= other.l
-    __gt__  = lambda self, other: self.l > other.l
-    __ge__  = lambda self, other: self.l >= other.l
+    def _cmp(self, other, op):
+        try:
+            return op(self.l, other.l)
+        except:
+            if op in [operator.ne, operator.gt, operator.lt]:
+                return True
+            return False
+
+    __eq__  = lambda self, other: self._cmp(other, operator.eq)
+    __ne__  = lambda self, other: self._cmp(other, operator.ne)
+    __lt__  = lambda self, other: self._cmp(other, operator.lt)
+    __le__  = lambda self, other: self._cmp(other, operator.le)
+    __gt__  = lambda self, other: self._cmp(other, operator.gt)
+    __ge__  = lambda self, other: self._cmp(other, operator.ge)
 
     def __hash__(self):
         return hash((self.l, self.h))
 
-def tile(s, bins, labels=False, retbins=True, infinite=True):
-    # 
+def _tile_inds(s, bins, labels=False, retbins=True, infinite=True):
+    #  NOTE: inf only happens when explicitly setting bins
+
+    # short circuit empty series
+    s = Series(s)
+    if s.count() == 0:
+        return np.repeat(None, len(s))
+
     if not np.iterable(bins):
         ind, label = cut(s, bins, retbins=retbins, labels=labels)
         # for now, pandas base cut doesn't support infinite ranges
@@ -47,8 +62,8 @@ def tile(s, bins, labels=False, retbins=True, infinite=True):
         ind = ind + 1
     else:
         bins = np.asarray(bins)
-        if (np.diff(bins) < 0).any():
-            raise ValueError('bins must increase monotonically.')
+        #if (np.diff(bins) < 0).any():
+        #    raise ValueError('bins must increase monotonically.')
         ind, label = inf_bins_to_cuts(s, bins)
     
 
@@ -62,23 +77,27 @@ def tile(s, bins, labels=False, retbins=True, infinite=True):
 
     if not infinite:
         na_mask = (ind == 0) | (ind == len(bins))
-        np.putmask(ind, na_mask, np.nan)
+        np.putmask(ind, na_mask, -1)
 
-    # redo the intindex as range index
-    new_index = ind.astype(object)
-    ind = Series(ind)
+    #ind = ind.astype(int)
+    ind[s.isnull().values] = -1
+    return Categorical(ind, ranges)
 
-
-    for k, v in ind.dropna().astype(int).iteritems():
-        newr = ranges[v]
-        new_index[k] = newr
-
+def tile(s, bins, labels=False, retbins=True, infinite=True):
+    new_index = _tile_inds(s, bins, labels=labels, retbins=retbins, infinite=infinite)
     grouped = s.groupby(new_index, sort=True)
     return grouped
 
 
+
 def inf_bins_to_cuts(x, bins, right=True, retbins=True,
                   precision=3, name=None, include_lowest=False):
+    """
+        Cut bins while supporting infinite. 
+        Regular pd.cut replaces out of range with nan. We support
+        positive and negative inf so you know which direction the value 
+        laid out. 
+    """
     if name is None and isinstance(x, Series):
         name = x.name
     x = np.asarray(x)
@@ -104,6 +123,6 @@ def tile_series(self, bins, series=None):
 @patch(DataFrame, 'tile')
 def tile_df(self, bins, col):
     series = self[col]
-    ind = cut(series, bins)
+    ind = _tile_inds(series, bins)
     return self.groupby(ind)
 
