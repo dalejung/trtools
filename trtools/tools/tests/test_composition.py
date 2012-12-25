b3dafd51d8b5ec33b3cc64850fad7ba55168bf1c
Dale Jung <dale@dalejung.com>
2012-12-25 04:16:08 -0500

    added test

---
+++ trtools/tools/tests/test_composition.py
@@ -0,0 +1,70 @@
+from unittest import TestCase
+import os.path
+
+import pandas as pd
+import numpy as np
+
+import trtools.util.testing as tm
+import trtools.tools.composition as composition
+reload(composition)
+UserSeries = composition.UserSeries
+UserFrame = composition.UserFrame
+
+def curpath():
+    pth, _ = os.path.split(os.path.abspath(__file__))
+    return pth
+
+class TestComposition(TestCase):
+
+    def __init__(self, *args, **kwargs):
+        TestCase.__init__(self, *args, **kwargs)
+
+    def runTest(self):
+        pass
+
+    def setUp(self):
+        pass
+
+    def test_user_series(self):
+        s = pd.Series(range(1, 10), index=range(11, 20))
+        us = UserSeries(s)
+        tm.assert_series_equal(s, us)
+
+        def assert_op(pobj, userobj, op):
+            return
+            cls = type(userobj)
+            correct = op(pobj)
+            test = op(userobj)
+            assert isinstance(test, cls)
+            if isinstance(correct, pd.Series):
+                tm.assert_series_equal(correct, test)
+            if isinstance(correct, pd.DataFrame):
+                tm.assert_frame_equal(correct, test)
+
+        assert_op(s, us, lambda s: s.pct_change())
+        assert_op(s, us, lambda s: s + 19)
+        assert_op(s, us, lambda s: s / 19)
+        assert_op(s, us, lambda s: np.log(s))
+        assert_op(s, us, lambda s: np.log(s))
+        assert_op(s, us, lambda s: np.diff(s))
+
+        bools = us > 5
+        tvals = np.repeat(1, len(us))
+        fvals = np.repeat(0, len(us))
+        wh = np.where(bools, tvals, fvals)
+        assert wh.pobj is not None
+        tm.assert_series_equal(wh, bools.astype(int))
+
+    def test_us_view(self):    
+        s = pd.Series(range(0, 10), index=range(10, 20))
+        us = s.view(UserSeries)
+        tm.assert_series_equal(s, us)
+
+        arr = np.array(range(10))
+        arrs = pd.Series(arr)
+        arrus = arr.view(UserSeries)
+        tm.assert_series_equal(arrs, arrus)
+
+if __name__ == '__main__':                                                                                          
+    import nose                                                                      
+    nose.runmodule(argv=[__file__,'-vvs','-x','--pdb', '--pdb-failure'],exit=False)   
