05d5372590def1684e13f57fef75505eeadd883f
Dale Jung <dale@dalejung.com>
2013-02-09 18:09:59 -0500

    made sure open and close are between high/low

--- trtools/util/testing.py
+++ trtools/util/testing.py
@@ -46,8 +46,8 @@ def fake_ohlc(N=1000, start="2000/01/01", freq="D"):
 
     open = 100 * geom
     close = open + (open * (np.random.random(N) - .5)) * .1
-    high = np.maximum(open, close)
-    low = np.minimum(open, close)
+    high = np.maximum(open, close) + .01
+    low = np.minimum(open, close) - .01
 
     df = pd.DataFrame(index=ind)
     df['open'] = open
