89e5f394fe3aca1323240224adca658c658174f1
Dale Jung <dale@dalejung.com>
2013-02-27 05:05:07 -0500

    added fake volume

--- trtools/util/testing.py
+++ trtools/util/testing.py
@@ -48,12 +48,14 @@ def fake_ohlc(N=1000, start="2000/01/01", freq="D"):
     close = open + (open * (np.random.random(N) - .5)) * .1
     high = np.maximum(open, close) + .01
     low = np.minimum(open, close) - .01
+    vol = 10000 + np.random.random(N) * 10000
 
     df = pd.DataFrame(index=ind)
     df['open'] = open
     df['high'] = high
     df['low'] = low
     df['close'] = close
+    df['vol'] = vol.astype(int)
     return df
 
 def assert_columnpanel_equal(left, right):
