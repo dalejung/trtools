ef7fd3e260646cbc1abd16434e7e99c40b94f47d
Dale Jung <dale@dalejung.com>
2013-03-11 01:52:40 -0400

    just simple mixin

---
+++ trtools/tools/mixin.py
@@ -0,0 +1,12 @@
+# http://c2.com/cgi/wiki?MixinsForPython
+def mixIn (base, addition):
+    """Mixes in place, i.e. the base class is modified.
+    Tags the class with a list of names of mixed members.
+    """
+    assert not hasattr(base, '_mixed_')
+    mixed = []
+    for item, val in addition.__dict__.items():
+        if not hasattr(base, item):
+            setattr(base, item, val)
+            mixed.append (item)
+    base._mixed_ = mixed
