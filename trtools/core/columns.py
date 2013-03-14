48e0eb5b551bb1b7c7aedd0b7d5267c87b86cb73
Dale Jung <dale@dalejung.com>
2013-03-14 18:09:28 -0400

    added ability to handle Index of objects. Fleshed out the tetsts better

--- trtools/core/columns.py
+++ trtools/core/columns.py
@@ -11,19 +11,74 @@ import pandas as pd
 
 from trtools.monkey import patch_prop
 
-class MultiIndexGetter(object):
+class IndexGetter(object):
     def __init__(self, obj, attr='columns'):
         self._obj = obj
         self._index = getattr(obj, attr)
 
     def __getattr__(self, name):
-        if name in self._index.names:
-            return self._index.get_level_values(name)
-        raise AttributeError()
+        if name in self.names:
+            return self.sub_column(name)
+        raise AttributeError(name)
+
+    def sub_column(self, name):
+        """
+        Return the value of name for every item in obj.columns
+        """
+        raise NotImplementedError('Implement this in subclass')
+
+    @property
+    def names(self):
+        raise NotImplementedError('Implement this in subclass')
+
+class MultiIndexGetter(IndexGetter):
+    """
+    Handles MultiIndex. 
+    Requires that that the levels be named.
+    """
+    def sub_column(self, name):
+        return self._index.get_level_values(name)
+
+    @property
+    def names(self):
+        """
+        Complete based off of MultiIndex.names
+        """
+        return [c for c in self._index.names]
+
+def _get_val(obj, name):
+    if isinstance(obj, dict):
+        return obj[name]
+    return getattr(obj, name)
+
+class ObjectIndexGetter(IndexGetter):
+    """
+    Handles an Index of objects and treats the attributes like un-ordered levels. 
+    """
+    def sub_column(self, name):
+        return pd.Index([_get_val(col, name) for col in self._obj.columns])
+
+    @property
+    def names(self):
+        """
+        Try to grab the proper attrs for the Columns
+        Best case is that the object has a keys method.
+        """
+        test = self._index[0]
+        try:
+            names = test.keys()
+            return names
+        except:
+            names = test.__dict__.keys()
+            return names
 
 def _getter(obj):
     if isinstance(obj.columns, pd.MultiIndex):
         return MultiIndexGetter(obj)
+
+    test = obj.columns[0]
+    if isinstance(obj.columns, pd.Index) and not np.isscalar(test):
+        return ObjectIndexGetter(obj)
 
 @patch_prop([pd.DataFrame], 'col')
 def col(self):
@@ -38,7 +93,7 @@ def install_ipython_completers():  # pragma: no cover
 
     @complete_object.when_type(MultiIndexGetter)
     def complete_column_panel_items(obj, prev_completions):
-        return prev_completions + [c for c in obj._index.names \
+        return prev_completions + [c for c in obj.names \
                     if isinstance(c, basestring) and py3compat.isidentifier(c)]                                          
 
 # Importing IPython brings in about 200 modules, so we want to avoid it unless
--- trtools/core/tests/test_columns.py
+++ trtools/core/tests/test_columns.py
@@ -5,7 +5,7 @@ import pandas as pd
 import numpy as np
 
 import trtools.util.testing as tm
-from trtools.core.columns import MultiIndexGetter
+from trtools.core.columns import MultiIndexGetter, ObjectIndexGetter
 
 limit = range(10)
 stop = range(3,7)
@@ -14,15 +14,60 @@ target = range(10, 20)
 sets = itertools.product(limit, stop, target)
 sets = list(sets)
 
-mi = pd.MultiIndex.from_tuples(sets)
-N = len(mi)
+N = len(sets)
 ind = pd.date_range(start="2000-01-01", freq="D", periods=N)
 
-df = pd.DataFrame(np.random.randn(N, N), columns=mi, index=ind)
-df.columns.names = ['limit', 'stop', 'target']
-m = MultiIndexGetter(df)
 
-class TestColumns(TestCase):
+class TestMultiIndexGetter(TestCase):
+
+    def __init__(self, *args, **kwargs):
+        TestCase.__init__(self, *args, **kwargs)
+
+    def runTest(self):
+        pass
+
+    def setUp(self):
+        mi = pd.MultiIndex.from_tuples(sets)
+        df = pd.DataFrame(np.random.randn(N, N), index=ind)
+        df.columns = mi
+        df.columns.names = ['limit', 'stop', 'target']
+        m = MultiIndexGetter(df)
+        self.df = df
+        self.m = m
+
+    def test_multiindexgetter(self):
+        m = self.m
+        df = self.df
+        test = df.ix[:, (m.limit == 0) & (m.stop == 3)]
+        assert len(test.columns) == 10 # should just be one row of targets
+
+    def test_property_col(self):
+        df = self.df
+        test = df.ix[:, (df.col.limit == 0) & (df.col.stop == 3)]
+        assert len(test.columns) == 10 # should just be one row of targets
+
+def dict_cols():
+    df = pd.DataFrame(np.random.randn(N, N), index=ind)
+    cols = [dict(zip(('limit', 'stop', 'target'), vals)) for vals in sets]
+    df.columns = cols
+    ogetter = ObjectIndexGetter(df)
+    return df, ogetter
+
+def object_cols():
+    class Object(object):
+        def __init__(self, dct):
+            for k, v in dct.items():
+                setattr(self, k, v)
+
+    dicts = [dict(zip(('limit', 'stop', 'target'), vals)) for vals in sets]
+    cols = [Object(d)  for d in dicts]
+
+    df = pd.DataFrame(np.random.randn(N, N), index=ind)
+    df.columns = cols
+    ogetter = ObjectIndexGetter(df)
+    return df, ogetter
+
+class TestObjectIndexGetter(TestCase):
 
     def __init__(self, *args, **kwargs):
         TestCase.__init__(self, *args, **kwargs)
@@ -33,13 +78,31 @@ class TestColumns(TestCase):
     def setUp(self):
         pass
 
-    def test_multiindexgetter(self):
-        test = df.ix[:, (m.limit == 0) & (m.stop == 3)]
-        assert len(test.columns) == 10 # should just be one row of targets
+    def test_names_keys(self):
+        # this uses __dict__ names
+        df, ogetter = dict_cols()
+        col = df.columns[0]
+        assert hasattr(col, 'keys') # dict has uses keys()
+        assert set(ogetter.names) == set(['limit', 'stop', 'target'])
 
-    def test_property_cx(self):
-        test = df.ix[:, (df.col.limit == 0) & (df.col.stop == 3)]
-        assert len(test.columns) == 10 # should just be one row of targets
+    def test_names__dict__(self):
+        # this uses __dict__ names
+        df, ogetter = object_cols()
+        col = df.columns[0]
+        assert not hasattr(col, 'keys') # Object has no keys method
+        assert set(ogetter.names) == set(['limit', 'stop', 'target'])
+
+    def test_col(self):
+        df, ogetter = object_cols()
+        stops = df.col.stop
+        for i, s in enumerate(sets):
+            assert stops[i] == s[1] # stop is second value of tuple
+
+        # test dicts
+        df, ogetter = dict_cols()
+        targets = df.col.target
+        for i, s in enumerate(sets):
+            assert targets[i] == s[2] # targets is third value of tuple
 
 if __name__ == '__main__':                                                                                          
     import nose                                                                      
