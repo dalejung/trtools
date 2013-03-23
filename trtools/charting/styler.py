0ed19c836fe15f29a8cb80cb98a2d20ebbbef19c
Dale Jung <dale@dalejung.com>
2013-03-23 03:45:45 -0400

    level styler

--- trtools/charting/styler.py
+++ trtools/charting/styler.py
@@ -1,11 +1,17 @@
 import itertools
+from collections import OrderedDict
+from pandas.core.algorithms import factorize
+import numpy as np
+
+LINESTYLES = ('-', '--', ':')
+COLORS = ('b', 'g', 'r', 'c', 'm', 'y', 'k')
+MARKERS = ('o', 's', 'v', '*', '^', 'x')
 
 def styler():
-    linestyles = ['-', '--', ':']
-    colors = ('b', 'g', 'r', 'c', 'm', 'y', 'k')
 
-    styles = itertools.product(linestyles, colors)
+    styles = itertools.product(LINESTYLES, COLORS)
 
+    # cycle through
     while True:
         yield dict(zip(('linestyle', 'color'), styles.next()))
 
@@ -13,10 +19,41 @@ def marker_styler():
     """
     Adds differing markers
     """
-    linestyles = ['-', '--', ':']
-    colors = ('b', 'g', 'r', 'c', 'm', 'y', 'k')
+    styles = itertools.product(LINESTYLES, COLORS, MARKERS)
 
-    styles = itertools.product(linestyles, colors)
-
+    # cycle through
     while True:
-        yield dict(zip(('linestyle', 'color'), styles.next()))
+        yield dict(zip(('linestyle', 'color', 'markers'), styles.next()))
+
+class StyleCategory(object):
+    def __init__(self, name, values):
+        self.name = name
+        self.values = values
+
+STYLES = {}
+STYLES['linestyle'] = LINESTYLES
+STYLES['color'] = COLORS
+STYLES['marker'] = MARKERS
+
+def level_styler(linestyle=None, color=None, marker=None):
+    """
+    This function is useful for categorical plotting. Based on certain categories,
+    it will return styles that are persistant. 
+
+    This is when you want to distinguish groups of line plots by their style
+    """
+    vars = locals().copy()
+
+    styles = OrderedDict()
+
+    for k, SC in STYLES.items():
+        vals = vars.get(k, None)
+        if vals is None:
+            continue
+        labels, uniques = factorize(vals)
+        labels = labels % len(SC) # cycle back to start
+        style_values = np.take(SC, labels)
+        styles[k] = style_values
+
+    keys = styles.keys()
+    return [dict(zip(keys, st)) for st in itertools.izip(*styles.values())]
