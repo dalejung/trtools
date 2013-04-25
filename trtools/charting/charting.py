07b603c3b9a4d28270b2ac84b85ac7a0324cdc5a
Dale Jung <dale@dalejung.com>
2013-04-25 19:40:48 -0400

    added the start of boxplot enhancements. moved code from private Gist

--- trtools/charting/charting.py
+++ trtools/charting/charting.py
@@ -313,6 +313,13 @@ class Grapher(object):
         self.df = df
 
     def boxplot(self, df, axis=0, secondary_y=False, *args, **kwargs):
+        """
+            Currently supports plotting DataFrames.
+
+            Downside is that this only works for data that has equal columns. 
+            For something like plotting groups with varying sizes, you'd
+            need to use boxplot(list()). Example is creating a SeriesGroupBy.boxplot
+        """
         if axis == 1:
             df = df.T
         index = df.columns 
@@ -327,6 +334,9 @@ class Grapher(object):
         ax.boxplot(clean_values, positions=np.arange(len(index)))
         self.setup_datetime(index)
         self.set_formatter()
+
+    def boxplot_list(self, data, secondary_y=False, *args, **kwargs):
+        pass
 
     def set_formatter(self):
         """ quick call to reset locator/formatter when lost. i.e. boxplot """
@@ -505,3 +515,27 @@ class DataFramePlotNS(PlotNS):
     @staticmethod
     def ohlc_plot(self, *args, **kwargs):
         ohlc_plot(*args, **kwargs)
+
+# TODO SeriesByGroupBy.boxplot
+"""
+import matplotlib.ticker as ticker
+
+labels = []
+data = []
+for label, group in grouped:
+        labels.append(label)
+            data.append(group)
+r = labels
+N = len(r)
+ind = np.arange(N)  # the evenly spaced plot indices
+def format_date(x, pos=None):
+        thisind = np.clip(int(x+0.5), 0, N-1)
+            return r[thisind].strftime('%Y-%m-%d')
+
+        fig = gcf()
+        ax = gca()
+        ax.xaxis.set_major_formatter(ticker.FuncFormatter(format_date))
+        _ = boxplot(data) 
+        fig.autofmt_xdate()
+"""
+
