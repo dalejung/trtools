9684d8a5420c1ab5462bee7db9c798883b501d33
Dale Jung <dale@dalejung.com>
2013-05-16 00:04:43 -0400

    added shortcut for pobj

--- trtools/api.py
+++ trtools/api.py
@@ -4,3 +4,6 @@ from trtools.pandasdb.api import *
 import trtools.util as util
 from trtools.tools.composition import UserSeries, UserFrame
 import trtools.tools.tile
+
+# create shortcut for core.base.generic_wrap
+pobj = generic_wrap
