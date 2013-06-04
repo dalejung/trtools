3336ff9f9bd5b5701732a93144546c7aa0fc6aed
Dale Jung <dale@dalejung.com>
2013-06-04 02:30:06 -0400

    MultiIndex.to_frame money patch

--- trtools/core/api.py
+++ trtools/core/api.py
@@ -6,6 +6,7 @@ import trtools.core.groupby # monkey
 import trtools.core.select # monkey
 import trtools.core.wrangling # monkey
 import trtools.core.rolling # monkey
+import trtools.core.indexing # monkey
 import trtools.core.time_selector # monkey
 import trtools.core.timeseries as timeseries
 from trtools.core.base import *
