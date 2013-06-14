f28a33494f44c704d44fa4b6643cc8f6a69f0c3e
Dale Jung <dale@dalejung.com>
2013-06-14 01:59:13 -0400

    got DataImport to work with in process cache

--- trtools/data_import.py
+++ trtools/data_import.py
@@ -1,68 +1,159 @@
+from collections import OrderedDict
+import types
+import ast
 import imp
 import sys
 import imputil
 
 # http://www.python.org/dev/peps/pep-0302/
 
-MODULE_CACHE = {}
+MODULE_VAR_CACHE = {}
+
+def _get_di_vars(code):
+    data = OrderedDict()
+    for node in code.body:
+        if isinstance(node, ast.Assign):
+            try:
+                names = [n.id for n in node.targets]
+                value = ast.literal_eval(node.value)
+            except:
+                continue
+
+            names = [name for name in names if name.startswith('DATAIMPORT')]
+            for name in names:
+                data[name] = value
+    return data
+
+class DataImportLoader(imputil.Importer):
+    def __init__(self, fullname, path):
+        self.fullname = fullname
+        self.path = path
+        mname = fullname.rpartition('.')[-1]
+        self.mname = mname
+
+    def load_module(self, fullname):
+        """
+        Note:
+        -----
+        We do not cache module in sys.modules
+        """
+        mname = self.mname
+        fp, pathname, desc = imp.find_module(mname, self.path)
+        suffix, mode, type = desc
+
+        source = fp.read()
+        code = ast.parse(source, pathname)
+        config = _get_di_vars(code)
+        # LOAD CACHE
+        cache = self.load_cache(fullname)
+        # process the ast and remove the cache vars
+        code = self.process_ast(code, config, cache)
+
+        code = compile(code, pathname, 'exec')
+        mod = self.new_module()
+        ns = {}
+        # populate namespace with cache vars
+        ns.update(cache)
+        exec code in ns
+        mod.__dict__.update(ns)
+
+        # SAVE CACHE
+        self.save_cache(ns)
+        return mod
+
+    # in process cache
+    def load_cache(self, config):
+        cache = MODULE_VAR_CACHE.get(self.fullname, {})
+        return cache
+
+    def save_cache(self, vars):
+        SKIP_TYPES = (types.ModuleType, types.FunctionType, types.LambdaType, 
+                     types.ClassType, types.FileType)
+        vars = {k:v for k, v in vars.iteritems() 
+                if not isinstance(v, SKIP_TYPES)}
+        vars.pop('__builtins__', None)
+        MODULE_VAR_CACHE[self.fullname] = vars
+
+    def process_ast(self, code, config, cache):
+        new_body = skip_nodes_in_cache(code.body, cache)
+        code.body = new_body
+        return code
+
+    def new_module(self):
+        mname = self.mname
+        mod = imp.new_module(mname)
+        mod.__path__ = self.path
+        return mod
 
 class DataImportFinder(object):
-    def __init__(self, *args):
-        pass
+    def __init__(self, loader_class=None):
+        if loader_class is None:
+            loader_class = DataImportLoader
+        self.loader_class = loader_class
  
     def find_module(self, fullname, path=None):
+        """
+        Only match DataImport modules
+        """
         mname = fullname.rpartition('.')[-1]
         try:
-            res = imp.find_module(mname, path)
+            fp, pathname, desc = imp.find_module(mname, path)
+            suffix, mode, type = desc
         except ImportError:
             return None
 
-        loader = DataImportLoader(mname, path)
+        # skip packages or non .py files
+        if fp is None or type != imp.PY_SOURCE:
+            return None
+
+        source = fp.read()
+        code = ast.parse(source, pathname)
+        config = _get_di_vars(code)
+
+        # no dataimport variables
+        if len(config) == 0:
+            return None
+
+        loader = self.loader_class(fullname, path)
         return loader
 
-class DataImportLoader(imputil.Importer):
-    def __init__(self, mname, path):
-        self.mname = mname
-        self.path = path
- 
-    def load_module(self, fullname):
-        # first time load normally
-        if fullname not in sys.modules:
-            module = self.load_first(fullname)
-            return module
+def skip_nodes_in_cache(nodes, cache):
+    # remove the assign statements that were gotten 
+    # from cache
+    new_body = []
+    for node in nodes:
+        res = _skip_node(node, cache)
+        if not res:
+            new_body.append(node)
+    return new_body
 
-        module = sys.modules[fullname]
-        return module
+def _skip_node(node, cache):
+    if not isinstance(node, ast.Assign):
+        return False
 
-    def load_first(self, fullname):
-        mname = self.mname
-        fp, pathname, description = imp.find_module(mname, self.path)
+    # only handle single assignments
+    if len(node.targets) > 1:
+        return False
 
-        module = imp.load_module(mname, fp, pathname, description)
+    try:
+        name = node.targets[0].id
+    except:
+        return False
 
-        sys.modules[fullname] = module
-        return module
+    if name in cache:
+        return True
 
-def _get_source(mname, path=None):
-    fp, pathname, desc = imp.find_module(mname, [path])
-    if fp:
-        source = fp.read()
-        return source
 
-def _get_dataimport_vars(source):
-    lines = source.splitlines()
-    di_lines = []
-    for line in lines:
-        if line.startswith('DATAIMPORT_'):
-            di_lines.append(line)
-        else:
-            break
+def install_data_import(loader_class=None):
+    sys.meta_path = [DataImportFinder(loader_class)]
 
-    di_source = '\n'.join(di_lines)
-    di_vars = {}
-    exec di_source in di_vars
-    return di_vars
- 
-def install_data_import():
-    sys.meta_path = [DataImportFinder()]
-
+"""
+Check if cache has data
+if cache:
+    load from cache
+    create_new_module
+    update module dict
+else:
+    load module
+    save cache 
+"""
