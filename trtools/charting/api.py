3971923af19b660f7c750d94b2a9eb5983c8e607
Dale Jung <dale@dalejung.com>
2013-06-20 19:51:02 -0400

    added span highlighting

--- trtools/charting/api.py
+++ trtools/charting/api.py
@@ -2,3 +2,4 @@ from trtools.charting.charting import *
 from trtools.charting.imagefile import *
 from trtools.charting.styler import *
 from trtools.charting.plot_3d import *
+from trtools.charting.span import *
