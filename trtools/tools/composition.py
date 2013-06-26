5b2bd758a88f478d685bd09c6c55d32d5c800d2e
Dale Jung <dale@dalejung.com>
2013-06-25 21:08:26 -0400

    mispelt name

--- trtools/tools/composition.py
+++ trtools/tools/composition.py
@@ -1,12 +1,8 @@
-import pandas as pd
-import numpy as np
-
 from trtools.monkey import AttrProxy, AttrNameSpace
 from pandas_composition import *
 import pandas_composition.base as base
 
-
-def attrns_handler(self, anme):
+def attrns_handler(self, name):
     return AttrProxy(name, self.pobj, lambda obj, full: self._wrap(full))
 
 base.WRAP_HANDLERS.insert(0, (AttrNameSpace, attrns_handler))
